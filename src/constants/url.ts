4b865cb5bdf0927daba3b89e61d274b915a3011b
Khulkar <mukhamadraimova@mail.ru>
2024-02-29 11:04:35 +0600

    change new URL

--- src/constants/url.ts
+++ src/constants/url.ts
@@ -1,2 +1,2 @@
 // export const API_URL='http://localhost:8055/'
-export const API_URL='https://ca71-93-185-74-170.ngrok-free.app/'
+export const API_URL='https://4ec9-93-185-74-170.ngrok-free.app/'
